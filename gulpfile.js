45fb194bdf3655af154a5c4795f7e1c094118f6f
Eve Urbanowska <eve.urbanowska@experian.com>
2016-05-20 14:54:04 +0100

    git 2

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,20 @@
 var gulp = require('gulp');
     gutil = require('gulp-util');
+    coffee = require('gulp-coffee');
 
-gulp.task('log', function(){
+   
+var coffeeSources = ['components/coffee/tagline.coffe'];
+
+
+    gulp.task('coffee', function(){
+        
+        gulp.src(coffeeSources)
+            .pipe(coffe({bare: true})
+                  .on('error', gutil.log))
+            .pipe(gulp.dest('components/scripts'))
+            
+                
+    });
+
     
-    gutil.log('workflows are awesome');
     
-    
-    
-    
-});
