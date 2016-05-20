88631f5d0b4527b605027a0731512046bda33bb9
Eve Urbanowska <eve.urbanowska@experian.com>
2016-05-20 15:11:59 +0100

    add js script

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,34 @@
-var gulp = require('gulp');
-    gutil = require('gulp-util');
-    coffee = require('gulp-coffee');
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    coffee = require('gulp-coffee'),
+    concat = require('gulp-concat')
+    
 
    
 var coffeeSources = ['components/coffee/tagline.coffe'];
 
+var jsSources = [
+    'components/scripts/rclick.js',
+    'components/scripts/pixgrid.js',         
+    'components/scripts/tagline.js',         
+    'components/scripts/template.js'           
+];
 
-    gulp.task('coffee', function(){
+   
+gulp.task('coffee', function(){
         
         gulp.src(coffeeSources)
             .pipe(coffe({bare: true})
                   .on('error', gutil.log))
             .pipe(gulp.dest('components/scripts'))
-            
-                
-    });
+});
 
     
     
+gulp.task('js', function(){
+    
+    gulp.src(jsSources)
+        .pipe(concat('script.js'))
+        .pipe(gulp.dest('builds/development/js'))
+        
+})
